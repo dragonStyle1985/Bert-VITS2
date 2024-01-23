44581ea861ae835ee620f3bff39d6ba8fde9d7d2
Yumo <dragon.tyl@163.com>
2024-01-23 20:13:44 +0800

    确保text_process能够生成正确的list文件

--- text_process.py
+++ text_process.py
@@ -1,20 +1,20 @@
 import os
 
-out_file = f"filelists/田豫龙-红军不怕远征难.list"
+folder_name = '田豫龙-红军不怕远征难'
+out_file = f"filelists/{folder_name}.list"
 
 
 def process():
     with open(out_file, 'w', encoding="Utf-8") as wf:
-        ch_name = '田豫龙'   # 不需要和list的文件名一致
         ch_language = 'ZH'  # ZH, EN
-        path = f"./raw/{ch_name}"
+        path = f"./raw/{folder_name}"
         files = os.listdir(path)
         sorted_files = sorted(files, key=lambda x: int(''.join(filter(str.isdigit, x))))
         for f in sorted_files:
             if f.endswith(".lab"):
                 with open(os.path.join(path, f), 'r', encoding="utf-8") as perFile:
                     line = perFile.readline()
-                    result = f"./dataset/{ch_name}/{f.split('.')[0]}.wav|{ch_name}|{ch_language}|{line}"
+                    result = f"./dataset/{folder_name}/{f.split('.')[0]}.wav|{folder_name}|{ch_language}|{line}"
                     wf.write(f"{result}\n")
 
 
